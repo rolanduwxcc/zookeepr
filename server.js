491753c9ce4294e685468e755be04655cd9edeaa
rolanduwxcc <rolanduwxcc@gmail.com>
2021-02-24 20:35:35 -0600

    hooked up the frontend

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();    //instantiates the server
 const { animals } = require('./data/animals.json');
+
+//-----------the app.use lines are all MIDDLEWARE
 //-----NEXT 4 LINES HAVE TO BE SETUP EVERY TIME TO ACCEPT POST DATA
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
 //parse incoming JSON data
 app.use(express.json());
+//MAKE SUPPORT FILES PUBLIC SO WE DONT HAVE TO MAKE A TON OF ROUTES
+app.use(express.static('public'));
 
 
 //-----------------------------------------------------FUNCTIONS
@@ -103,6 +107,18 @@ app.post('/api/animals', (req, res) => {
 });
 
 //---------------------------------------------------GET ROUTES
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+
 app.get('/api/animals', (req, res) => {
     // res.send('Meow!');
     let results = animals;
